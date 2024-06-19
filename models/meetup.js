7f17a13f72560e32e2174a83710fc71a6cd9479b
Hulahup01 <ilya.deepak@yandex.ru>
2024-06-19 10:19:24 +0300

    add models

---
+++ models/meetup.js
@@ -0,0 +1,53 @@
+const sequelize = require('../db');
+const { DataTypes } = require('sequelize');
+const { Tag } = require('./tag');
+
+
+const Meetup = sequelize.define('meetup', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+
+    time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+
+    location: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: false
+});
+
+
+const MeetupTag = sequelize.define('meetup_tag', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    }
+}, {
+    timestamps: false
+});
+
+Meetup.belongsToMany(Tag, {through: MeetupTag});
+Tag.belongsToMany(Meetup, {through: MeetupTag});
+
+
+module.exports = { Meetup } 
