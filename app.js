99d87196ded3cf67aeb7bb04a11f11acac12e270
Hulahup01 <ilya.deepak@yandex.ru>
2024-06-28 14:56:35 +0300

    cors fix

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ const PORT = process.env.PORT;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: '*',
+    headers: '*',
+}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(passport.initialize());
